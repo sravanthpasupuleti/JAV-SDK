87cd9a374c4fcba03a2deb71b411f67f28099101
sravanthpasupuleti <sravanthpasupuleti.gmail.com>
2025-08-10 08:11:10 +0530

    streams

--- JAVA-8/StreamsProblems/Sortnums.java
+++ JAVA-8/StreamsProblems/Sortnums.java
@@ -1,9 +1,13 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Sortnums {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(9 , 3, 21, 5, 16,7, 8, 9);
         List<Integer> sort = nums.stream()
-        .sorted();
+        .map(num -> num)
+        .sorted()
+        .collect(Collectors.toList());
+        System.out.println(sort);
     }
 }
